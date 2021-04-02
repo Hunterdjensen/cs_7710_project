296d3b86c47d80775d9ebe54db41525f6bbf2d2b
Hunterdjensen <38997640+Hunterdjensen@users.noreply.github.com>
2021-04-02 11:28:42 -0600

    Added 1024 ImageNet validation images, and updated run_pytorch.py to interface with them and report an accuracy

--- class_labels.py
+++ class_labels.py
@@ -2,7 +2,9 @@ import torch
 
 
 #################################################################################################
-#     Reads in 'imagenet_classes.txt' and interfaces with it for viewing label predictions      #
+#     Reads in 'imagenet_classes.txt' and interfaces with it for viewing label predictions.     #
+#     Also reads 'val.txt' and creates a dictionary so that image file names can map to         #
+#     their ground-truth labels.                                                                #
 #################################################################################################
 
 # Read in the labels from 'imagenet_classes.txt'
@@ -12,17 +14,45 @@ with open('imagenet_classes.txt') as f:
         line = line.split(',')
         class_name = line[1].strip()    # Name of class, line[0] contains line number
         labels.append(class_name)
-# print("imagenet_classes read in successfully.")
+
+val_labels = {}
+with open('val.txt') as f:
+    for line in f:
+        filename, label = line.split()
+        val_labels[filename] = int(label)
 
 
 # Define function for printing out the results
-def print_top_5(output):
+def print_top_5(output, correct_labels=None):
     images_per_batch, num_classes = output.shape
     percentages = torch.nn.functional.softmax(output, dim=1) * 100
     _, indices = torch.sort(output, descending=True)
 
     for image in range(images_per_batch):
+        if correct_labels is not None:
+            print("Correct label is: ", labels[correct_labels[image]])
+
         for i in range(5):
             idx = indices[image][i]     # Index of top class
             print('{:<1d}:{:>8.2f}%  {:<8s}'.format(i+1, percentages[image][idx].item(), labels[idx]))
         print()     # Newline
+
+
+# Access val_labels
+def get_label(file):
+    return val_labels[file]
+
+
+# Compare predictions to correct labels, and return the number correct
+def get_num_correct(output, correct_labels):
+    images_per_batch, num_classes = output.shape
+    percentages = torch.nn.functional.softmax(output, dim=1) * 100
+    _, indices = torch.sort(output, descending=True)
+    num_correct = 0
+
+    for image in range(images_per_batch):
+        prediction = indices[image][0]  # [0] for the largest prediction
+        if prediction == correct_labels[image]:
+            num_correct += 1
+
+    return num_correct
--- run_pytorch.py
+++ run_pytorch.py
@@ -3,32 +3,47 @@ import torchvision.models as models
 from torchvision import datasets, transforms as T
 from PIL import Image
 import numpy as np
+import random
+import os   # For accessing files
 from class_labels import print_top_5
+from class_labels import get_label
+from class_labels import get_num_correct
 
 
-# Define the transform for our images - resizes/crops them to 256x256, normalizes (required for ImageNet)
+# Define the transform for our images - resizes/crops them to 224x224, normalizes (required for ImageNet)
 transform = T.Compose([
     T.Resize(256),
-    T.CenterCrop(256),
+    T.CenterCrop(224),
     T.ToTensor(),
     T.Normalize(mean=[0.485, 0.456, 0.406],
                 std=[0.229, 0.224, 0.225])
 ])
 
 # Instantiate the model
-mobilenet = models.mobilenet_v3_small(pretrained=True)
-mobilenet.eval()    # Put the model in inference mode
-# print(mobilenet)  # Prints architecture (num layers, etc.)
+net = models.mobilenet_v3_small(pretrained=True)
+net.eval()    # Put the model in inference mode
+# print(net)  # Prints architecture (num layers, etc.)
 
-# Load two images, prepare in a batch
-img1 = Image.open("ILSVRC2017_test_00000004.JPEG")
-img2 = Image.open("ILSVRC2017_test_00000017.JPEG")
-img1_t = transform(img1)
-img2_t = transform(img2)
-# batch_t = torch.unsqueeze(img1_t, 0)  # Use for a single-image batch
-batch_t = torch.stack((img1_t, img2_t), dim=0)  # Batch 2 images together
-print("batch shape: ", batch_t.shape)
 
-# Run the network
-out = mobilenet(batch_t)    # out has shape [N, 1000] where N = batch size
-print_top_5(out)            # Print out the predictions
+# Run the network for a fixed number of epochs and print the accuracy
+num_epochs = 4
+batch_size = 64
+total_correct = 0
+for epoch in range(num_epochs):
+    # Load images and prepare them in a batch
+    image_dir = 'val/'
+    random_files = random.sample(os.listdir(image_dir), batch_size)
+    gt_labels = [get_label(file) for file in random_files]  # Ground-truth label for each img
+
+    batch_t = torch.empty((batch_size, 3, 224, 224))    # [N, C, H, W]
+    for i in range(batch_size):
+        img = Image.open(image_dir + '/' + random_files[i]).convert("RGB")
+        img_t = transform(img)
+        batch_t[i,:,:,:] = img_t
+
+    # Run the network
+    out = net(batch_t)    # out has shape [N, 1000] where N = batch size\
+    num_correct = get_num_correct(out, gt_labels)
+    total_correct += num_correct
+    print("Epoch %d:  %d / %d" % (epoch, num_correct, batch_size))
+print("Percentage Correct: %.2f%%" % ((total_correct/(batch_size*num_epochs))*100))
